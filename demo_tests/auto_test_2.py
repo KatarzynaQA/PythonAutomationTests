7246caecedc4adaf3551b04336c7fe736525816f
Katarzyna Kusiak (PGS Software) <kkusiak@pgs-soft.com>
2019-05-29 09:55:46 +0200

    add different tests from part two

--- demo_tests/auto_test_2.py
+++ demo_tests/auto_test_2.py
@@ -3,5 +3,28 @@ from selenium import webdriver
 
 
 class MainTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.driver = webdriver.Chrome(executable_path=r"C:\drivers\chromedriver.exe")
+
+
     def test_demo_login(self):
-        pass
+        driver = self.driver
+        driver.get("https://autodemo.testoneo.com/en/")
+        title = driver.title
+        print(title)
+        assert 'Lost Hat' == title
+
+
+    def test_demo_account(self):
+        driver = self.driver
+        driver.get("https://demobank.jaktestowac.pl/konta.html")
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Konta' == title
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
+
+
